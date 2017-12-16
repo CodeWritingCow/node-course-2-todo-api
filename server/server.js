479604b1b2b2c9f768b2c588c5c4c8cf4b8d1997
Gary Pang <gary.pang@gmail.com>
2017-12-16 21:03:50 +0000

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -112,21 +112,23 @@ app.post('/users', (req, res) => {
     .catch((err) => {
         res.status(400).send(err);
     });
-})
+});
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-    // var token = req.header('x-auth');
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
     
-    // User.findByToken(token).then((user) => {
-    //     if(!user) {
-    //         return Promise.reject();
-    //     }
-        
-    //     res.send(user);
-    // }).catch((err) => {
-    //     res.status(401).send();
-    // });
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, process.env.IP, () => {
